bb01f7b3dcc5f44a169bb3ff7b8371417273a2a6
Faithful Chris <75838035+Heyfruz@users.noreply.github.com>
2022-11-10 08:47:08 +0100

    Boilerplate & Dev Environment Set Up
    Typescript, Babel, & Linting Configurations for workspace

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,24 @@
 module.exports = {
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          assets: './app/assets',
+          components: './app/components',
+          constant: './app/constant',
+          core: './app/core',
+          hooks: './app/hooks',
+          navigation: './app/navigation',
+          screens: './app/screens',
+          store: './app/store',
+          utils: './app/utils',
+        },
+        extensions: ['.js', '.ts', '.tsx'],
+        root: ['./'],
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
   presets: ['module:metro-react-native-babel-preset'],
 };
